6c9e616ff8ba04f13763d7cbf859b15e6f5c11a5
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-08-09 17:04:17 +0600

    get single room

--- index.js
+++ index.js
@@ -86,6 +86,14 @@ async function run() {
       res.send(result)
     })
 
+    // get a single room
+    app.get('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await roomsCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
