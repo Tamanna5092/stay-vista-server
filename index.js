ac771683ec9c12b2837ab3b6d6e521e3588134a0
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-08-16 02:14:36 +0600

    delete a room

--- index.js
+++ index.js
@@ -105,6 +105,14 @@ async function run() {
       const result = await roomsCollection.findOne(query)
       res.send(result)
     })
+    
+    // delete a room
+    app.delete('/room/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await roomsCollection.deleteOne(query)
+      res.send(result)
+    })
 
  
     // Get all rooms for host
