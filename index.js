9b57fe80629ec51ae32610c15ef166d3268d48c0
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2025-09-22 11:24:12 +0600

    all bookings

--- index.js
+++ index.js
@@ -253,6 +253,14 @@ async function run() {
       res.send(result)
     })
 
+    // get all bookings for a guest
+    app.get('/my-bookings/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      const query = { 'guest.email': email }
+      const result = await bookingsCollection.find(query).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
